92e11ea7936903efd5492524652679c63f36e41b
Sashko Potapov <opotapov@ucu.edu.ua>
2020-10-14 22:35:49 +1100

    Added PostgreSQL driver and small example

--- src/main/java/ucu/learning/App.java
+++ src/main/java/ucu/learning/App.java
@@ -1,11 +1,23 @@
 package ucu.learning;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        final String dbUrl = "jdbc:postgresql://localhost:5432/foundation";
+        try (final Connection conn = DriverManager.getConnection(dbUrl, "dbuser", "dbpassw0rd");
+             final ResultSet rs = conn.createStatement().executeQuery("select * from person_");) {
+            while (rs.next()) {
+                System.out.printf("Person: %s %s%n", rs.getString("name_"), rs.getString("surname_")); 
+            }
+        }
     }
 }
